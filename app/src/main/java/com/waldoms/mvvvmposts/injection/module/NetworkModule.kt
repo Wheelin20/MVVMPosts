90b151215346a61e8d02b1fd51d760affacad4a1
Kevin Emigh <kevin@waldoms.com>
2018-11-03 00:13:17 -0400

    adds function provideRetrofitInterface to NetworkModule

--- app/src/main/java/com/waldoms/mvvvmposts/injection/module/NetworkModule.kt
+++ app/src/main/java/com/waldoms/mvvvmposts/injection/module/NetworkModule.kt
@@ -3,9 +3,13 @@ package com.waldoms.mvvvmposts.injection.module
 import dagger.Module
 import dagger.Provides
 import dagger.Reusable
+import io.reactivex.schedulers.Schedulers
 import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
 
 import com.waldoms.mvvvmposts.network.PostApi
+import com.waldoms.mvvvmposts.utils.BASE_URL
 
 @Module
 @Suppress("unused")
@@ -17,5 +21,17 @@ object NetworkModule
     internal fun providePostApi(retrofit: Retrofit): PostApi
     {
         return retrofit.create(PostApi::class.java)
+    }
+
+    @Provides
+    @Reusable
+    @JvmStatic
+    internal fun provideRetrofitInterface(): Retrofit
+    {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.createWithScheduler(Schedulers.io()))
+            .build()
     }
 }
