768109fb4ad50aa73ddccf84e71269c5f41667ff
Kevin Emigh <kevin@waldoms.com>
2018-11-02 23:46:04 -0400

    adds retrofit libraries

--- app/src/main/java/com/waldoms/mvvvmposts/model/Post.kt
+++ app/src/main/java/com/waldoms/mvvvmposts/model/Post.kt
@@ -1,3 +1,3 @@
 package com.waldoms.mvvvmposts.model
 
-data class Post(val userId:Int val id:Int, val title:String, val body:String)
+data class Post(val userId:Int, val id:Int, val title:String, val body:String)
